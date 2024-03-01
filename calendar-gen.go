c561e95b5dfff76a72f6bed8a785219d5d284d79
Mark Bradley <markbradley27@gmail.com>
2024-02-29 19:01:26 -0500

    Add last day of month

--- calendar-gen.go
+++ calendar-gen.go
@@ -29,17 +29,7 @@ func (e *event) row() []string {
 	return []string{e.subject, e.startDate, strconv.FormatBool(e.allDayEvent)}
 }
 
-type events struct {
-	events []event
-}
-
-func (e *events) add(date time.Time) {
-	e.events = append(e.events, event{
-		subject:     "Payday!",
-		startDate:   date.Format("01-02-2006"),
-		allDayEvent: true,
-	})
-}
+type events []event
 
 func (e *events) csv(w io.Writer) error {
 	csvW := csv.NewWriter(w)
@@ -47,7 +37,7 @@ func (e *events) csv(w io.Writer) error {
 	if err := csvW.Write([]string{"Subject", "Start Date", "All Day Event"}); err != nil {
 		return err
 	}
-	for _, event := range e.events {
+	for _, event := range *e {
 		if err := csvW.Write(event.row()); err != nil {
 			return err
 		}
@@ -56,22 +46,53 @@ func (e *events) csv(w io.Writer) error {
 	return nil
 }
 
-func main() {
-	events := events{}
+func aalyriaPayday() (events events) {
+	addEvent := func(date time.Time) {
+		events = append(events, event{
+			subject:     "Payday!",
+			startDate:   date.Format("01-02-2006"),
+			allDayEvent: true,
+		})
+	}
 
 	now := time.Now()
 	year, month, _ := now.Date()
 	for year < 2026 {
 		midDay := fridayBeforeIfWeekend(time.Date(year, month, 15, 0, 0, 0, 0, now.Location()))
-		events.add(midDay)
+		addEvent(midDay)
 
 		lastDay := fridayBeforeIfWeekend(time.Date(year, month, 1, 0, 0, 0, 0, now.Location()).AddDate(0, 1, -1))
-		events.add(lastDay)
+		addEvent(lastDay)
 
 		month++
 		year += int(month) / 13
 		month = ((month - 1) % 12) + 1
 	}
+	return
+}
+
+func lastDayOfMonth(subject string) (events events) {
+	now := time.Now()
+	year, month, _ := now.Date()
+	for year < 2026 {
+		lastDayOfMonth := time.Date(year, month, 1, 0, 0, 0, 0, now.Location()).AddDate(0, 1, -1)
+
+		events = append(events, event{
+			subject:     subject,
+			startDate:   lastDayOfMonth.Format("01-02-2006"),
+			allDayEvent: true,
+		})
+
+		month++
+		year += int(month) / 13
+		month = ((month - 1) % 12) + 1
+	}
+	return
+}
+
+func main() {
+	// events := aalyriaPayday()
+	events := lastDayOfMonth("$ - Casella Trash Bill Due")
 
 	if err := events.csv(os.Stdout); err != nil {
 		log.Printf("csv: %v", err)
